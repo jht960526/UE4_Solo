12124348981ea7e8cb722850012c8e146755888d
sfml_game <jht960526@naver.com>
2021-06-26 04:02:35 +0900

    chapter2
    오브젝트생성
    createdefaultUobject
    static mesh, teplate set
    ...

---
+++ Source/UE4_SoloPro/Fountain.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Fountain.generated.h"
+
+UCLASS()
+class UE4_SOLOPRO_API AFountain : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFountain();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Body;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent* Light; // 조명 기능
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* Splash; // 파티클
+
+};
