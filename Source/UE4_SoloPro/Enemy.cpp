f886e01f1ff9a3bda5b571d425fd3cdafaea184d
sfml_game <jht960526@naver.com>
2021-08-01 21:47:53 +0900

    Enemy Combat 3(68)
    Add Debug Sphere
    Add explain for AINav

--- Source/UE4_SoloPro/Enemy.cpp
+++ Source/UE4_SoloPro/Enemy.cpp
@@ -5,6 +5,7 @@
 #include "Components/SphereComponent.h"
 #include "AIController.h"
 #include "Main.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 AEnemy::AEnemy()
@@ -83,11 +84,23 @@ void AEnemy::MoveToTarget(AMain* Target)
 	{
 		FAIMoveRequest MoveRequest;
 		MoveRequest.SetGoalActor(Target);
-		MoveRequest.SetAcceptanceRadius(5.0f);
+		MoveRequest.SetAcceptanceRadius(25.0f); //목표지점에 얼마나 가까워지면 성공인지
 
-		FNavPathSharedPtr NavPath; // 길정보
+		FNavPathSharedPtr NavPath; // Enemy와 타겟의 움직인 길정보들이 저장됨
 
-		AIController->MoveTo(MoveRequest, &NavPath);
+		AIController->MoveTo(MoveRequest, &NavPath); // 함수가 실행되고 나서 NavPath에 정보를 저장
+
+		/*
+		auto PathPoints = NavPath->GetPathPoints(); //TArray<FNavPathPoint>가 타입
+
+		for(auto Point : PathPoints)
+		{
+			FVector Location = Point.Location;
+
+			UKismetSystemLibrary::DrawDebugSphere(this, Location, 25.f,8,FLinearColor::Red, 10.f, 1.5f);
+			// 어떤 오브젝트, 위치, 지름, 몇각형, 색깔, 지속시간, 두께
+		}
+		*/
 	}
 }
 
